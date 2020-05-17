10ba35e3277820ac4702b2fd72aee4ed26a95065
juanjo <jponzvan@mercadona.es>
2020-05-17 20:36:06 +0200

    Working on not ack messages when have not messages

--- lib/postoffice/application.ex
+++ lib/postoffice/application.ex
@@ -19,12 +19,10 @@ defmodule Postoffice.Application do
       {Phoenix.PubSub, [name: Postoffice.PubSub, adapter: Phoenix.PubSub.PG2]},
       {Cluster.Supervisor,
        [Application.get_env(:libcluster, :topologies), [name: Postoffice.ClusterSupervisor]]},
-      Postoffice.PublisherProducer,
-      Postoffice.MessagesProducerSupervisor,
-      Postoffice.Rescuer.Producer,
-      Postoffice.Rescuer.Supervisor,
-      {Cachex, :retry_cache},
-      Postoffice.Cachex
+      # Postoffice.PublisherProducer,
+      # Postoffice.MessagesProducerSupervisor,
+      # Postoffice.Rescuer.Producer,
+      # Postoffice.Rescuer.Supervisor
     ]
 
     :ok =
