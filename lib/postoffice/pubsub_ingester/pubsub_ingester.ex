0180eb35e61e45b1a548f641d08d66062146f0a4
Daniel Ruiz <druizca@mercadona.es>
2020-06-05 21:36:57 +0200

    Change subscription prefix with an env variable PubSubIngester can change its subscription prefix without downtime

--- lib/postoffice/pubsub_ingester/pubsub_ingester.ex
+++ lib/postoffice/pubsub_ingester/pubsub_ingester.ex
@@ -3,11 +3,11 @@ defmodule Postoffice.PubSubIngester.PubSubIngester do
 
   def run(%{topic: topic_name, pubsub_topic: pubsub_topic_name}) do
     conn = PubSubClient.connect()
-    PubSubClient.get(conn, %{topic: topic_name, sub: "#{impl}-#{pubsub_topic_name}"})
+    PubSubClient.get(conn, %{topic: topic_name, sub: "#{subscription_prefix}-#{pubsub_topic_name}"})
     |> case do
       {:ok, messages} ->
         ingest_messages({:ok, messages})
-        |> confirm(conn, "#{impl}-#{pubsub_topic_name}")
+        |> confirm(conn, "#{subscription_prefix}-#{pubsub_topic_name}")
 
       error ->
         error
@@ -26,5 +26,5 @@ defmodule Postoffice.PubSubIngester.PubSubIngester do
   defp confirm({:ok, []}, _conn, _sub_name), do: {:ok, []}
 
   defp confirm(ackIds, conn, sub_name), do: PubSubClient.confirm(conn, sub_name, ackIds)
-  defp impl, do: "prefix"
+  defp subscription_prefix, do: System.get_env("SUBSCRIPTION_PREFIX")
 end
