d4da351cf0c81f58cca240b6746b86cf2d3099be
Daniel Ruiz <druizca@mercadona.es>
2020-06-22 19:47:38 +0200

    Change function used to get a message from db

--- lib/postoffice_web/controllers/api/search_controller.ex
+++ lib/postoffice_web/controllers/api/search_controller.ex
@@ -1,11 +1,10 @@
 defmodule PostofficeWeb.Api.SearchController do
   use PostofficeWeb, :controller
 
-  alias Postoffice.Messaging
   alias Postoffice.Messaging.Message
 
   def show(conn, %{"id" => id}) do
-    case Messaging.get_message!(id) do
+    case Postoffice.get_message(id) do
       %Message{} = message -> render(conn, "message.json", message: message)
       _ ->
         conn
