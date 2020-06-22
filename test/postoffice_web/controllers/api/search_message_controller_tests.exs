d7b70563f57fa46b9fd230c296031358725bb1cc
Daniel Ruiz <druizca@mercadona.es>
2020-06-22 19:45:38 +0200

    Remove unused alias in test code

--- test/postoffice_web/controllers/api/search_message_controller_tests.exs
+++ test/postoffice_web/controllers/api/search_message_controller_tests.exs
@@ -1,8 +1,6 @@
 defmodule PostofficeWeb.Api.SearchControllerTest do
   use PostofficeWeb.ConnCase, async: true
 
-  alias Postoffice.Messaging
-
   describe "the search endpoint" do
     test "returns 404 when requested with a message id that is not found", %{conn: conn} do
       conn = get(conn, Routes.api_search_path(conn, :show, 999))
